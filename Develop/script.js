4928ab476efc71a0d4950685394ec92fd98dd25a
SadiaSookram <sadiamsookram@gmail.com>
2021-07-09 10:15:52 -0400

    Seperated prompt statements and true values

--- Develop/script.js
+++ Develop/script.js
@@ -11,36 +11,39 @@ var generateBtn = document.querySelector("#generate");
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Generate Password
-for (var i = 0; i < passwordLen; i++){
+// User window criteria (true or false)
 
-  var uppercaseConfirm = window.confirm("Would you like password to have Upper Case Letters?");
-  // If yes add upperCase letter
-  if (uppercaseConfirm) {
-    upperCase = Math.floor(Math.random()*upperCase.length);
+ var uppercaseConfirm = window.confirm("Would you like password to have Upper Case Letters?"); 
  
    
-  var lowercaseConfirm = window.confirm("Would you like passowrd to have Lower Case Letters?");
-  // If yes add LowerCase letter
-  if (lowercaseConfirm) {
-    lowerCase = Math.floor(Math.random()*lowerCase.length);
-    
-  var numberselConfirm = window.confirm("Would you like your oassword to have Numbers?");
-  // If yes add numbers
-  if (numberselConfirm) {
-    numberSel = Math.floor(Math.random()*numberSel.length);
-
-  var symbolsconfirm = window.confirm("Do you want Symbols in your password ?");
-  if (symbolsconfirm) {
-    symbols = Math.floor(Math.random()*symbols.length)
-  }
+ var lowercaseConfirm = window.confirm("Would you like passowrd to have Lower Case Letters?");
  
-  passwordText.value = password(numberSel, symbols,passwordLen,upperCase,lowerCase);
-  generateBtn.addEventListener("click", writePassword);
-}
-}
-  }
-};
+   
+ var numberselConfirm = window.confirm("Would you like your password to have Numbers?");
+  
+
+ var symbolsconfirm = window.confirm("Do you want Symbols in your password ?");
+    
+
+
+
+//Generator Functions if window is confirmed
+
+if (uppercaseConfirm === true) {
+  var upperCase = Math.floor(Math.random()*upperCase.length);}
+
+if (lowercaseConfirm === true) {
+    var lowerCase = Math.floor(Math.random()*lowerCase.length);}
+
+if (numberselConfirm === true) {
+      var numberSel = Math.floor(Math.random()*numberSel.length);}
+
+if (symbolsconfirm === true) {
+      var symbols = Math.floor(Math.random()*symbols.length);}
+
+        var passowrd= "";
+// for loop writing password 
+for (var i = 0; i < passwordLen; i++){}
 
 
 // Write password to the #password input
