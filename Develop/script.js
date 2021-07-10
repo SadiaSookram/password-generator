09398f579422833063cc0ff7c894bd20b0865911
SadiaSookram <sadiamsookram@gmail.com>
2021-07-10 15:21:08 -0400

    Added corrected true statmetns , forloop added, return value to screen

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,23 @@
-
 // Variables used for Password generator
 var generateBtn = ""
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var numberSel = ["0","1","2","3","4","5","6","7","8","9"]
-var symbols = ['"','!','#','$','%','&','(',')','*','+','-','.','/',';','<','=','>','?','@','[',']','^','_','{','|','}','~']
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var numberSel = "0123456789"
+var symbols = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~'"
 var passwordLen = (8,128)
-var passwordText = document.querySelector("#password");
-var generateBtn = document.querySelector("#generate");
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var password =''
+
 
 
 // User window criteria (true or false)
  
  var passwordLen = window.prompt("Please choose Password length between 8-128 characters ");
-
+ if (passwordLen < 8 || passwordLen > 128){
+   alert ("Please enter in valid amount.")
+ }
 
  var uppercaseConfirm = window.confirm("Would you like password to have Upper Case Letters?"); 
  
@@ -34,48 +34,42 @@ var password =''
 
 function generatePassword(){
 
-//Console log for password fuctions
-console.log('passwordLen', passwordLen);
-console.log('uppercaseConfirm', uppercaseConfirm);
-console.log('lowercaseConfirm', lowercaseConfirm);
-console.log('numberselConfirm',numberselConfirm );
-console.log('symbolsConfirm', symbolsConfirm);
+  //Console log for password fuctions
+  console.log('passwordLen', passwordLen);
+  console.log('uppercaseConfirm', uppercaseConfirm);
+  console.log('lowercaseConfirm', lowercaseConfirm);
+  console.log('numberselConfirm',numberselConfirm );
+  console.log('symbolsConfirm', symbolsConfirm);
 
-//Mandatory charaters & Possible characters to include
-var allPossibleCharArray = [];
-var mandatoryCharArray = [];
+  //Mandatory charaters & Possible characters to include
+  var allPossibleCharArray = "";
 
+  //if user confirms window prompts 
+  if (uppercaseConfirm === true) {
+    allPossibleCharArray=allPossibleCharArray.concat(upperCase);
+  }
+  if (lowercaseConfirm === true) {
+    allPossibleCharArray=allPossibleCharArray.concat(lowerCase);
+  }
+  if (numberselConfirm === true) {
+    allPossibleCharArray=allPossibleCharArray.concat(numberSel);
+  }
+  if (symbolsConfirm === true) {
+    allPossibleCharArray=allPossibleCharArray.concat(symbols);
+  }
 
-//if user confirms window prompts then ...
-if (uppercaseConfirm === true) {
-  allPossibleCharArray=allPossibleCharArray.concat(upperCase);
-console.log("allPossibleCharArray",allPossibleCharArray)
-    var upperCaseIndex = Math.floor(Math.random()*upperCase.length);
-    mandatoryCharArray.push(upperCase[upperCaseIndex])
-    console.log("mandatoryCharArray",mandatoryCharArray) 
-    
-if (lowercaseConfirm === true) {
-  allPossibleCharArray=allPossibleCharArray.concat(lowerCase);
-console.log("allPossibleCharArray",allPossibleCharArray)
-    var lowerCaseIndex = Math.floor(Math.random()*lowerCase.length);
-    mandatoryCharArray.push(lowerCase[lowerCaseIndex])
-    console.log("mandatoryCharArray",mandatoryCharArray)
+  console.log("allPossibleCharArray",allPossibleCharArray);
+   
+  // Random Generator through for loop 
+  var password =''
 
-if (numberSelConfirm === true) {
-      allPossibleCharArray=allPossibleCharArray.concat(numberSel);
-    console.log("allPossibleCharArray",allPossibleCharArray)
-        var numberSelIndex = Math.floor(Math.random()*numberSel.length);
-        mandatoryCharArray.push(numberlSel[numberSelIndex])
-        console.log("mandatoryCharArray",mandatoryCharArray)
-
-if (symbolsConfirm === true) {
-          allPossibleCharArray=allPossibleCharArray.concat(symbols);
-        console.log("allPossibleCharArray",allPossibleCharArray)
-            var symbolsIndex = Math.floor(Math.random()*symbols.length);
-            mandatoryCharArray.push(symbols[symbolsIndex])
-            console.log("mandatoryCharArray",mandatoryCharArray)
-
-}
+  for (i = 0; i < passwordLen; i++){
+    password += allPossibleCharArray.charAt(Math.floor(Math.random()*allPossibleCharArray.length));
+  } 
+  console.log("password", password );
+  return password;
+  var passwordText = document.querySelector("#password");
+  var generateBtn = document.querySelector("#generate");
 }
 
 // Add event listener to generate button
