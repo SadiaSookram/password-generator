0d8de12216bb5e037d50bc2825df839d4df4dec2
SadiaSookram <sadiamsookram@gmail.com>
2021-07-10 01:45:59 -0400

    Including all possible char options for new arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,18 @@
 
 // Variables used for Password generator
 var generateBtn = ""
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var numberSel = "0123456789";
-var symbols = "!#$%&()*+,-./:;<=>?@[\]^_{|}~";
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var numberSel = ["0","1","2","3","4","5","6","7","8","9"]
+var symbols = ['"','!','#','$','%','&','(',')','*','+','-','.','/',';','<','=','>','?','@','[',']','^','_','{','|','}','~']
 var passwordLen = (8,128)
 var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var password ='';
+var password =''
 
-//Generate password & for loop
-
-function generatePassword(upperCase , lowerCase, numberSel, symbol, passwordlen){
-
-}
 
 // User window criteria (true or false)
  
@@ -33,21 +28,55 @@ function generatePassword(upperCase , lowerCase, numberSel, symbol, passwordlen)
  var numberselConfirm = window.confirm("Would you like your password to have Numbers?");
   
 
- var symbolsconfirm = window.confirm("Do you want Symbols in your password ?");
+ var symbolsConfirm = window.confirm("Do you want Symbols in your password ?");
 
- //Generator Functions if window is confirmed
+//Generate password functions using arrays if window is confirmed
 
+function generatePassword(){
+
+//Console log for password fuctions
+console.log('passwordLen', passwordLen);
+console.log('uppercaseConfirm', uppercaseConfirm);
+console.log('lowercaseConfirm', lowercaseConfirm);
+console.log('numberselConfirm',numberselConfirm );
+console.log('symbolsConfirm', symbolsConfirm);
+
+//Mandatory charaters & Possible characters to include
+var allPossibleCharArray = [];
+var mandatoryCharArray = [];
+
+
+//if user confirms window prompts then ...
 if (uppercaseConfirm === true) {
-  var upperCase = Math.floor(Math.random()*upperCase.length);}
-  
+  allPossibleCharArray=allPossibleCharArray.concat(upperCase);
+console.log("allPossibleCharArray",allPossibleCharArray)
+    var upperCaseIndex = Math.floor(Math.random()*upperCase.length);
+    mandatoryCharArray.push(upperCase[upperCaseIndex])
+    console.log("mandatoryCharArray",mandatoryCharArray) 
+    
 if (lowercaseConfirm === true) {
-    var lowerCase = Math.floor(Math.random()*lowerCase.length);}
+  allPossibleCharArray=allPossibleCharArray.concat(lowerCase);
+console.log("allPossibleCharArray",allPossibleCharArray)
+    var lowerCaseIndex = Math.floor(Math.random()*lowerCase.length);
+    mandatoryCharArray.push(lowerCase[lowerCaseIndex])
+    console.log("mandatoryCharArray",mandatoryCharArray)
 
-if (numberselConfirm === true) {
-      var numberSel = Math.floor(Math.random()*numberSel.length);}
+if (numberSelConfirm === true) {
+      allPossibleCharArray=allPossibleCharArray.concat(numberSel);
+    console.log("allPossibleCharArray",allPossibleCharArray)
+        var numberSelIndex = Math.floor(Math.random()*numberSel.length);
+        mandatoryCharArray.push(numberlSel[numberSelIndex])
+        console.log("mandatoryCharArray",mandatoryCharArray)
 
-if (symbolsconfirm === true) {
-      var symbols = Math.floor(Math.random()*symbols.length);} 
+if (symbolsConfirm === true) {
+          allPossibleCharArray=allPossibleCharArray.concat(symbols);
+        console.log("allPossibleCharArray",allPossibleCharArray)
+            var symbolsIndex = Math.floor(Math.random()*symbols.length);
+            mandatoryCharArray.push(symbols[symbolsIndex])
+            console.log("mandatoryCharArray",mandatoryCharArray)
+
+}
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -58,5 +87,4 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;        
-
 }
